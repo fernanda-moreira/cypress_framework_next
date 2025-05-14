416173296bb9aed617842e43ee3389073fc2bd98
nique.alisson <alisson_201083@hotmail.com>
2025-05-14 14:16:59 -0300

    main

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://nextocasino.com', //baseUrl
+    viewportHeight:900,
+    viewportWidth:1440,
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    ],
+    defaultCommandTimeout: 120000, // Sets a higher timeout for commands
+    chromeWebSecurity: false, // Allows testing cross-origin iframes
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+    },
+  },
+});
