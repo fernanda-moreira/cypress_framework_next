27038b5a38ee78be448841d00beb1a3409db2055
nique.alisson <alisson_201083@hotmail.com>
2025-05-14 16:13:10 -0300

    atualizção TC001

--- cypress/e2e/tests/depositAndPayTest/TC125.cy.js
+++ cypress/e2e/tests/depositAndPayTest/TC125.cy.js
@@ -1,24 +1,24 @@
-import depositPay from '../../../support/pageObjects/depositPay';
-import LoginPage from '../../../support/pageObjects/LoginPage';
+//import depositPay from '../../../support/pageObjects/depositPay';
+//import LoginPage from '../../../support/pageObjects/LoginPage';
 
-describe('Navigation Bar Test', () => {
-    let users;
+//describe('Navigation Bar Test', () => {
+   // let users;
 
-    before(() => {
-        cy.fixture('users/users.json').then((data) => {
-            users = data;
-        });
-    });
+    //before(() => {
+       // cy.fixture('users/users.json').then((data) => {
+         //   users = data;
+        //});
+    //});
 
-    beforeEach(() => {
-        const validUser = users.validUser;
-        LoginPage.login(validUser.username, validUser.password); // Realiza o login antes de cada teste
+    //beforeEach(() => {
+        //const validUser = users.validUser;
+        //LoginPage.login(validUser.username, validUser.password); // Realiza o login antes de cada teste
 
-    });
-
-
+    //});
 
 
 
 
-});
+
+
+//});
