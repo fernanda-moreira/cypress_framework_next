cc753c86b1127bae27297141a0ba7c1b11789757
fernanda-moreira <smoreira.fernanda@gmail.com>
2025-05-21 18:47:49 +0300

    main

--- cypress/e2e/tests/mainPageTest/mainpage.cy.js
+++ cypress/e2e/tests/mainPageTest/mainpage.cy.js
@@ -6,7 +6,7 @@ describe('Navigation Bar Test', () => {
 
     before(() => {
         cy.fixture('users/users.json').then((data) => {
-            users = data;
+            users = data;   
         });
     });
 
@@ -14,13 +14,32 @@ describe('Navigation Bar Test', () => {
         const validUser = users.validUser;
         LoginPage.login(validUser.username, validUser.password); 
     });
-    it('should navigate through all links in the navigation bar', () => {
+
+    // TC001 - Logged in navigation test
+    it('TC001 - should navigate through all links in the navigation bar', () => {
         MainPage.verificaLinkCasino();
-        MainPage.verificaLinkCasinoAoVivo();
+        //MainPage.verificaLinkCasinoAoVivo();
         MainPage.verificaLinkEsportes();
         MainPage.verificaLinkLivespins();
         MainPage.verificaLinkPromocoes();
         MainPage.verificaLinkCalendario();
     });
+
+    // TC002 - Guest navigation test for Slot Games
+    it('TC002 - should navigate to Slot Games page as guest', () => {
+    cy.clearCookies(); // Ensure no session
+    cy.visit('/');     // Start from home page
+    MainPage.verificaLinkSlotGamesGuest();
+    });
+
+
+    // // ✅ TC002 - Gaming Menu Options Clickability
+    // it('TC002 - Menu options are clickable and redirect correctly', () => {
+    //     MainPage.checkGamingOptionsMenuLinks();
+    // });
+
+    // // ✅ TC003 - In-page Game Category Navigation
+    // it('TC003 - In-page game category navigation scrolls correctly', () => {
+    //     MainPage.checkCategoryNavigation();
+    // });
 });
- 
--- cypress/support/pageObjects/mainPage.js
+++ cypress/support/pageObjects/mainPage.js
@@ -4,7 +4,7 @@ class MainPage {
         cy.get('button.mr-6.xl\\:mr-0.xl\\:hidden').should('be.visible').click();
     }
 
-    // Navigation Bar Links (TC001)
+    // TC001 - Navigation Bar Links
     verificaLinkCasino() {
         this.openMenu();
         cy.get('a[href="/casino"]:visible').first().click();
@@ -13,7 +13,7 @@ class MainPage {
 
     verificaLinkCasinoAoVivo() {
         this.openMenu();
-        cy.get('a[href="/casino/live_casino"]:visible').first().click();
+        cy.get('a[href="/live_casino"]:visible').first().click();
         cy.url().should('include', '/casino/live_casino');
     }
 
@@ -41,110 +41,43 @@ class MainPage {
         cy.url().should('include', '/calendar');
     }
 
-    // TC002
-    checkGamingOptionsButtons() {
-        cy.get('.gaming-options button').each(($btn) => {
-            cy.wrap($btn).should('be.visible').click();
-        });
+    verificaLinkAllGamesGuest() {
+        cy.get('a[href="/casino/all_games"]').click();
+        cy.url().should('include', '/casino/all_games');
     }
 
-    // TC003
-    checkCategoryNavigation() {
-        cy.get('.category-menu a').each(($link) => {
-            cy.wrap($link).should('be.visible').click();
-        });
-    }
 
-    // TC004 / TC028
-    checkDepositNowPopup() {
-        cy.get('.popup .deposit-now').should('be.visible').click();
-        cy.url().should('include', '/deposit');
-    }
 
-    // TC005 to TC010
-    checkFooterLink(selector, expectedUrlPart) {
-        cy.get(selector).should('be.visible').click();
-        cy.url().should('include', expectedUrlPart);
-    }
+//   // ✅ TC002 - Gaming Menu Options Clickability (Updated Style)
+// checkGamingOptionsMenuLinks() {
+//     const menuLinks = [
+//         '/all_games',
+//         '/slots',
+//         '/live_casino',
+//         '/casino/virtual_games',
+//         '/casino/top_table_and_crash_games',
+//         '/casino/lucky_games'
+//     ];
 
-    // TC011
-    checkHeaderVisible() {
-        cy.get('header').should('be.visible');
-    }
+//     menuLinks.forEach((path) => {
+//         cy.get(`a[href="${path}"]:visible`).first().click();
+//         cy.url().should('include', path);
+//         cy.log(`Verified menu link to: ${path}`);
+//         cy.visit('/casino'); // Return to base after each test
+//     });
+// }
 
-    // TC012
-    checkGamingOptionsBarVisible() {
-        cy.get('.gaming-options').should('be.visible');
-    }
-
-    // TC013
-    checkFooterVisible() {
-        cy.get('footer').should('be.visible');
-    }
-
-    // TC014
-    checkHoverEffects() {
-        cy.get('a').first().trigger('mouseover');
-        // Add hover effect check logic if styles change
-    }
-
-    // TC015
-    checkGameThumbnailsVisible() {
-        cy.get('.game-thumbnail').should('be.visible');
-    }
-
-    // TC016
-    checkSearchBarFunctionality(keyword) {
-        cy.get('input[type="search"]').type(keyword);
-        cy.get('button.search').click();
-        cy.get('.game-list').should('contain', keyword);
-    }
-
-    // TC017
-    checkGameProviderFilter() {
-        cy.get('.provider-filter select').select(1);
-        cy.get('.game-thumbnail').should('exist');
-    }
-
-    // TC018
-    checkSearchBarInputLimit(maxLength = 50) {
-        const longInput = 'a'.repeat(maxLength + 10);
-        cy.get('input[type="search"]').type(longInput).invoke('val').should('have.length.lte', maxLength);
-    }
-
-    // TC019
-    checkSearchBarNoResults() {
-        cy.get('input[type="search"]').clear().type('nonexistentgame');
-        cy.get('.no-results').should('be.visible');
-    }
-
-    // TC020
-    clickShowAll() {
-        cy.get('button.show-all').click();
-        cy.get('.game-list .game-item').should('have.length.greaterThan', 10);
-    }
-
-    // TC021
-    clickPlayButton() {
-        cy.get('.play-button').first().click();
-    }
-
-    // TC023
-    checkDefaultLanguage(expectedLang) {
-        cy.document().its('documentElement.lang').should('eq', expectedLang);
-    }
-
-    // TC024/025
-    checkCategorySubtitles() {
-        cy.get('.category-subtitle').each(($el) => {
-            cy.wrap($el).invoke('text').should('match', /\d+/);
-        });
-    }
-
-    // TC026
-    checkTranslatedContent(expectedText) {
-        cy.get('body').should('contain.text', expectedText);
-    }
+//     // ✅ TC003 - Game Category Navigation (Anchors inside the page)
+//     checkCategoryNavigation() {
+//         cy.get('.category-menu a[href^="#"]').each(($link) => {
+//             const href = $link.attr('href');
+//             if (href) {
+//                 cy.wrap($link).should('be.visible').click();
+//                 cy.get(href).should('exist').and('be.visible');
+//                 cy.log(`Navigated to in-page section: ${href}`);
+//             }
+//         });
+//     }
 }
 
 export default new MainPage();
